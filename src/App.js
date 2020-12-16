a458606fbbc1b522bd55d52d7335f9fbeb03942e
Oliver Starckjohann <oliverstarckjohann@gmail.com>
2020-12-16 09:33:02 +0100

    changed namings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React, { useState } from "react";
 import styled from "styled-components/macro";
 
-const TaskBoard = styled.div`
+let appDataSource = [{ datakey: "home" }, { datakey: "outside" }];
+
+export default function App() {
+  const [statedata, setStatedata] = useState(appDataSource);
+
+  statedata.forEach((dataitem) =>
+    console.log(`${dataitem.datakey}` + " from foreach")
+  );
+  statedata.map((dataitem) => console.log(`${dataitem.datakey}` + " from map"));
+
+  return (
+    <>
+      <ContentBoard className="App">
+        <h2>Content from stateArray:</h2>
+        {statedata.map((dataitem) => (
+          <p>{dataitem.datakey} maped from state (statedata)</p>
+        ))}
+        <p>{statedata[0].dataitem} direct from array accessed (indexvalue)</p>
+        <p>{statedata[1].dataitem} direct from array accessed (indexvalue)</p>
+      </ContentBoard>
+    </>
+  );
+}
+
+const ContentBoard = styled.div`
   font-family: sans-serif;
   width: 180px;
   background-color: #fbf6b9;
@@ -9,27 +33,3 @@ const TaskBoard = styled.div`
   margin-bottom: 10px;
   padding: 10px;
 `;
-
-let taskBoardData = [{ data: "home" }, { data: "outside" }];
-
-function App() {
-  const [todos, setTodos] = useState(taskBoardData);
-
-  todos.forEach((todo) => console.log(`${todo.data}` + " from foreach"));
-  todos.map((todo) => console.log(`${todo.data}` + " from map"));
-
-  return (
-    <>
-      <TaskBoard className="App">
-        <h2>Content from Object:</h2>
-        {todos.map((todo) => (
-          <p>{todo.data} maped from useState</p>
-        ))}
-        <p>{todos[0].data} direct from object accessed</p>
-        <p>{todos[1].data} direct from object accessed</p>
-      </TaskBoard>
-    </>
-  );
-}
-
-export default App;
